034650f10a04753285952cf40cc0975a1abdce61
Ruan <ruan@pysoft.co.za>
2026-03-29 21:09:44 +0200

    Expand testing

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@ size_t col(const char *src, const char *pos) {
 	return col;
 }
 
-bool test_tokens(const char *src, const struct token *tokens) {
+bool test_tokens(const char *src, const struct token *tokens, const struct lexer_error *errors) {
 	struct lexer_errors err = {0};
 	struct lexer lex = lexer_new(src);
 
@@ -57,8 +57,41 @@ bool test_tokens(const char *src, const struct token *tokens) {
 		sb_free(sb_gen);
 		sb_free(sb_cst);
 
-		if (lex.tok.type == TT_EOF) return true;
+		if (lex.tok.type == TT_EOF) break;
 	}
+
+	da_foreach(struct lexer_error, it, &err) {
+		if (errors->pos == NULL) {
+			printf("  Unexpected error:\n");
+			printf("  got @ %lu,%lu %s\n", line(src, it->pos), col(src, it->pos), it->msg);
+			printf("  expected no error\n");
+			return false;
+		}
+
+		if (errors->pos != it->pos) {
+			printf("  Got errors at wrong position:\n");
+			printf("  got      @ %lu,%lu %s\n", line(src, it->pos), col(src, it->pos), it->msg);
+			printf("  expected @ %lu,%lu %s\n", line(src, errors->pos), col(src, errors->pos), errors->msg);
+			return false;
+		}
+
+		if (strcmp(errors->msg, it->msg) != 0) {
+			printf("  Got unexpected errors message:\n");
+			printf("  got      @ %lu,%lu %s\n", line(src, it->pos), col(src, it->pos), it->msg);
+			printf("  expected @ %lu,%lu %s\n", line(src, errors->pos), col(src, errors->pos), errors->msg);
+			return false;
+		}
+
+		++errors;
+	}
+
+	if (errors->pos != NULL) {
+		printf("  Didn't get expected errors:\n");
+		printf("  expected @ %lu,%lu %s\n", line(src, errors->pos), col(src, errors->pos), errors->msg);
+		return false;
+	}
+
+	return true;
 }
 
 const char *const src0 = "42";
@@ -66,24 +99,38 @@ const struct token toks0[] = {
 	make_token(TT_NUM, src0 + 0, 2, .num = 42),
 	make_token(TT_EOF, src0 + 2, 0),
 };
+const struct lexer_error errs0[] = {{0}};
 const char *const src1 = "   1337   ";
 const struct token toks1[] = {
 	make_token(TT_NUM, src1 + 3, 4, .num = 1337),
 	make_token(TT_EOF, src1 + 10, 0),
 };
+const struct lexer_error errs1[] = {{0}};
 const char *const src2 = "18446744073709551615";
 const struct token toks2[] = {
 	make_token(TT_NUM, src2 + 0, 20, .num = ~0ul),
 	make_token(TT_EOF, src2 + 20, 0),
 };
+const struct lexer_error errs2[] = {{0}};
+const char *const src3 = "18446744073709551616";
+const struct token toks3[] = {
+	make_token(TT_NUM, src3 + 0, 20, .num = 0),
+	make_token(TT_EOF, src3 + 20, 0),
+};
+const struct lexer_error errs3[] = {
+	{ .pos = src3 + 19, .msg = "integer overflow while parsing number" },
+	{0},
+};
 
 struct {
 	const char *src;
 	const struct token *toks;
+	const struct lexer_error *errs;
 } tests[] = {
-	{ .src = src0, .toks = toks0 },
-	{ .src = src1, .toks = toks1 },
-	{ .src = src2, .toks = toks2 },
+	{ .src = src0, .toks = toks0, .errs = errs0 },
+	{ .src = src1, .toks = toks1, .errs = errs1 },
+	{ .src = src2, .toks = toks2, .errs = errs2 },
+	{ .src = src3, .toks = toks3, .errs = errs3 },
 };
 
 int main(int argc, char **argv) {
@@ -92,7 +139,7 @@ int main(int argc, char **argv) {
 
 	for (size_t i = 0; i < ARRAY_LEN(tests); ++i) {
 		printf("TEST %lu:\n", i);
-		puts(test_tokens(tests[i].src, tests[i].toks) ? "  PASS" : "  FAIL");
+		puts(test_tokens(tests[i].src, tests[i].toks, tests[i].errs) ? "  PASS" : "  FAIL");
 	}
 
 	const char *src = "18446744073709551616";
